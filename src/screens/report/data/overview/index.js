7fcdf1c608af21bfd61ae581b3a2c509e6a4fb01
Jonathan Choi <51044058+koreaunitas@users.noreply.github.com>
2022-02-28 10:37:46 +0900

    update

--- src/screens/report/data/overview/index.js
+++ src/screens/report/data/overview/index.js
@@ -2,8 +2,36 @@ export default {
     title: "Overview",
     contents: [
         {
-            title: "Intern Report @ CVLAB, 2022 Winter",
-            description: "Adding !"
+            title: "Abstract",
+            description: `I remember what I said when I first met the professor in the lab. "I'm going to explore deeply where we come from, 
+            where we should go, and how we communicate with the essence of our lives." The professor Kim said he wish to 
+            find the answer here. Did I find the answer? It was a short term, but I would say I can say the answer.`
+        },
+        {
+            title: "Briefly, I’ve done/am progressing on below things",
+            description: 
+            <p>Paper Study: I have read 35 papers across various fields during seminar and reviewed the paper review for all 6 
+            weeks. In addition, I created the Github repository to organize my studies and to provide useful contents to any 
+            visitor to my Github. (+not only about papers, but also organized codes related to the papers)
+            Coding Study: I studied about the Python, Pytorch, and other codes including Transformers, VAT, CATs etc. Like 
+            above, I also created the Github repository to organize my studies, that aiming to provide kind guide to any visitors 
+            who come to my Github.
+            Team Seminar / Meeting (Manipulation Team): During the research of ‘Style Transfer via INR’, I read 12 papers 
+            related to Image-to-image translation and Style transfer and wrote the writings to summarize the things that I’ve 
+            studied to my Github.
+            Lab Seminar: I’ve learned about the Managing projects with Git, VAT, the diffusion model and about the 3D
+            Geometric Variation from great lecturers.
+            ECCV 2022 (I2I Team): For the ECCV 2022, I read 8 papers to study deeply to I2I, especially with the 
+            unsupervised few-shot image-to-image translation to realize the multi-modal across multi-domains. Recently, I’m
+            drawing figures that will be contained in the paper with team.
+            Additional Projects and thoughts + Plans: In the end, all the things are connected: it comes from how we perceive
+            the world. By researching the I2I and Style Transfer that I’m studying in the lab, I will connect these dots with the 
+            cosmic fundamental dimension that I have desired for.
+            The below contents including:
+            - Figures: Screenshots of the Presentation, Github Writings, Repository, and the Web Projects.
+            - Links: The original links to connect directly to the contents
+            - Appendix: The additional contents to explain the background and works (Neuralverse)
+            </p>
         }
     ]
 }
